a9796b5cfdd88aaff62b26f081a331977174b88f
Pavitha P <pavithapari10@gmail.com>
2025-05-06 14:26:43 +0530

    Initial

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+void get_input() {
+    char name[10];
+    int symptoms[10];
+    int i = 0, num;
+    FILE *fp1;
+
+    printf("\nEnter your name: ");
+    scanf("%s", name);
+    printf("1.headache 2.fever 3.cough 4.stomach pain 5.skin rash 6.chest pain 7.fatigue 8.depression 9.shortness of breath");
+    printf("Enter the symptoms number and press 0 to end\n");
+
+    while (1) {
+        scanf("%d", &num);
+        if (num == 0) break;
+        if (i < 10) {
+            symptoms[i++] = num;
+        } else {
+            printf("Max 10 symptoms allowed.\n");
+            break;
+        }
+    }
+
+    fp1 = fopen("user_data.txt", "a");
+    if (fp1 != NULL) {
+        fprintf(fp1,"\n"); 
+        fprintf(fp1, "Name: %s\n", name); 
+        fprintf(fp1,"Symptoms:");
+        for (int j = 0; j < i; j++) {
+            fprintf(fp1, "%d ", symptoms[j]);  
+        }
+        fprintf(fp1, "\n");  
+        fclose(fp1);
+    } else {
+        printf("Error opening file!\n");
+    }
+}
+
+int main() {
+    printf("Hellow! Welcome to our symptoms checler!\n");
+    get_input();
+
+    printf("Patient data saved successfully!\n");
+
+    return 0;
+}
